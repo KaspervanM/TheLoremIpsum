eabd502eeb6bbf937acbad79a360a3b853bddb93
Kasper van Maasdam <kaspervanm@gmail.com>
2020-09-30 14:53:39 +0200

    SQL implementation preparation.
    The tools required to communicate with the MySQL server.

---
+++ DB_interface.php
@@ -0,0 +1,70 @@
+<?php
+	if (!isset($_POST["DB_interface"])) die();
+	$servername = "localhost:3306";
+	$username = "loremipsum";
+	$password = "password";
+	$DBname = "LoremIpsumDB";
+
+	$conn = new mysqli($servername, $username, $password, $DBname);
+
+	if ($conn->connect_error){
+		die("ERROR: Unable to connect: $conn->connect_error");
+	}
+
+	function insertUser($username, $password, $firstname, $infix, $lastname, $email){
+		global $conn;
+
+		$query = "SELECT 1 FROM Users WHERE username = '$username' OR email = '$email'";
+		if ($conn->query($query)->fetch_assoc()) {
+			die("STOPNOTE: User already exists.");
+		}
+
+		$query = "INSERT INTO Users (username, password, firstname, infix, lastname, email) VALUES ('$username', '$password', '$firstname', '$infix', '$lastname', '$email')";
+		if ($conn->query($query) === TRUE) {
+			die("SUCCESS: New record created successfully");
+		} else {
+			die("ERROR: $query <br> $conn->error");
+		}
+	}
+
+	function getUserData($user, $select = "*"){
+		global $conn;
+
+		$query = "SELECT $select FROM Users WHERE username = '$user' OR email = '$user'";
+		if ($result = $conn->query($query)) {
+			var_dump($result->fetch_assoc());
+			die();
+		}
+	}
+
+	function UserVerification($user, $password){
+		global $conn;
+
+		$query = "SELECT $select FROM Users WHERE username = '$user' OR email = '$user'";
+		if ($result = $conn->query($query)) {
+			var_dump($result->fetch_assoc());
+			die("STOPNOTE: User does not exist.");
+		}
+		
+		$query = "SELECT $select FROM Users WHERE (username = '$user' OR email = '$user') AND password='$password'";
+		if ($result = $conn->query($query)) {
+			var_dump($result->fetch_assoc());
+			die("SUCCESS: User verified.");
+		}
+		
+	}
+
+	switch (strtolower($_POST["DB_interface"])){
+		case "insertuser":
+				insertUser($_POST["username"], $_POST["password"], $_POST["firstname"], $_POST["infix"], $_POST["lastname"], $_POST["email"]);
+				break;
+		case "getuserdata":
+				getUserData($_POST["user"]);
+				break;
+		case "userverification":
+				getUserData($_POST["user"]);
+				break;
+		default:
+				die();
+	}
+?>
---
+++ instantiateDB.php
@@ -0,0 +1,61 @@
+<?php
+$username = "loremipsum";
+$password = "password";
+$dbName = "LoremIpsumDB";
+$servername = "localhost:3306";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: ".$conn->connect_error);
+}
+echo "Connected successfully<br/>";
+
+$conn->query("DROP DATABASE $dbName;");
+
+$query = "SHOW DATABASES LIKE '$dbName';";
+if (!$conn->query($query)->fetch_assoc()) {
+
+	$query = "CREATE DATABASE $dbName";
+
+	if ($conn->query($query)) {
+
+		echo "Database created successfully<br/>";
+	}else {
+		echo "Error creating database: " . $conn->error;
+	}
+} else {
+	echo "db exists!<br/>";
+}
+
+$conn->close();
+
+$conn = new mysqli($servername, $username, $password, $dbName);
+
+$query = "SHOW TABLES LIKE 'Users'";
+if (!$conn->query($query)->fetch_assoc()) {
+
+	$query = "CREATE TABLE Users (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(30),
+password VARCHAR(30),
+firstname VARCHAR(30) NOT NULL,
+infix VARCHAR(30),
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50) NOT NULL,
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+	if ($conn->query($query)) {
+
+		echo "Table 'Users' created successfully<br/>";
+	}else {
+		echo "Error creating table 'Users': " . $conn->error;
+	}
+} else {
+	echo "Table 'Users' exists!";
+}
+
+?>
