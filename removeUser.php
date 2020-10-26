6a6581c219db0158c19b4296a20d4718fdc8f1b8
Kasper van Maasdam <kaspervanm@gmail.com>
2020-10-26 09:17:28 +0100

    Create removeUser.php
    A way to remove users from the db and their directory from the server.

---
+++ removeUser.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Add User</title>
+		<meta charset="utf-8">
+		<script src="https://apis.google.com/js/platform.js" async defer></script>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" ></script>
+		<link rel="stylesheet" media="screen" href="css/css.css">
+		<link rel="stylesheet" media="screen" href="css/contact.css">
+		<link rel="icon" href="favicon.ico" type="image/x-icon">
+		<meta name="google-signin-client_id" content="967580510442-4fpfl1b3u31jmkki8df96g5r9ff5rqv9.apps.googleusercontent.com">
+	</head>
+	<body>
+		<form action="return false">
+			<input name="user" type="text" required placeholder="Username or E-mail address"/>
+			<input type="hidden" name="DB_interface" value="removeUser"/>
+			<br/>
+			<button action="submit"><span>Add</span></button>
+		</form><br/>
+		<p id="output"></p>
+		<script>
+		    var output = document.getElementById("output");
+			$("form").submit(function(event){
+				event.preventDefault();
+				var arr = $(this).serializeArray();
+				console.log(arr);
+				$.post("DB_interface", {DB_interface:arr[1]["value"], user:arr[0]["value"]},
+					function success(e){
+						console.log(e);
+						output.innerHTML = e;
+				});
+			});
+		</script>
+	</body>
+</html>
